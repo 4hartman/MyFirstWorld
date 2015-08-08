0ace8b0e9a91779c2cc89c074699101242b52871
Its me! <julius.94@hotmail.de>
2015-08-08 18:36:14 +0200

    all

---
+++ Assets/Scripts/CameraContoller.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraContoller : MonoBehaviour 
+{
+
+
+	public GameObject player;
+	
+	private Vector3 offset;
+	
+	void Start ()
+	{
+		offset = transform.position - player.transform.position;
+	}
+	
+	void Update ()
+	{
+		
+		Vector3 mausRotation = new Vector3 (Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"), 0);
+		Quaternion rotation = Quaternion.LookRotation(mausRotation);
+		
+		if (Input.GetMouseButtonDown(1))
+			transform.rotation = rotation;
+	}
+
+	void LateUpdate ()
+	{
+		//Vector3 startPos = new Vector3 (194, 7, 221);
+		//Vector3 relativePos = startPos - transform.position;
+
+
+		transform.position = player.transform.position + offset;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public float maxSpeed = 15;
+	public float jump = 1;
+
+	private Rigidbody rb;
+	
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody>();
+		rb.drag = 0;	
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+
+	void FixedUpdate ()
+	{
+		//if (Input.GetButtonDown ("Horizontal") || Input.GetButtonDown ("Vertical"))
+			rb.mass = 0.3f;
+
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+
+		if(rb.velocity.magnitude > maxSpeed)
+		{
+			rb.velocity = rb.velocity.normalized * maxSpeed;
+		}
+
+		rb.mass = 2.0f;
+
+		if (Input.GetButtonDown("Jump"))
+			rb.AddForce (0, jump, 0);
+
+	}
+}
